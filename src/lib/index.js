957d71a4d051cfd8630dd1948b86293bbe0d156e
Jaymar Westerlow <jaywesterlow71@gmail.com>
2025-03-31 06:31:31 +0200

    v2.1 | Playwright installation

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,6 +1,9 @@
 // Reexport your entry components here
 
 export { default as Svg } from './media/Svg.svelte'
+export { default as Svg } from './media/Carousel.svelte'
+
 export { default as LoginHeader } from './navigation/Login-header.svelte'
+
 export { default as Link01 } from './inputs/Link-01.svelte'
 // export { default as Component } from './path/Component.svelte'
