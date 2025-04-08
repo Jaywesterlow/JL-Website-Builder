0695b53709328a38ded7926a68ecece8fffe9561
Jaymar Westerlow <jaywesterlow71@gmail.com>
2025-04-08 07:57:34 +0200

    v2.5 Svg & Language button setup

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,9 +1,10 @@
 // Reexport your entry components here
 
 export { default as Svg } from './media/Svg.svelte'
-export { default as Svg } from './media/Carousel.svelte'
+export { default as Carousel } from './media/Carousel.svelte'
 
 export { default as LoginHeader } from './navigation/Login-header.svelte'
 
 export { default as Link01 } from './inputs/Link-01.svelte'
+export { default as Language } from './inputs/Language.svelte'
 // export { default as Component } from './path/Component.svelte'
