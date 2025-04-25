f2e6565eec0ddab015c35ad28cac87d90110246d
Jaymar Westerlow <jaywesterlow71@gmail.com>
2025-04-25 05:48:21 +0200

    v3.3.1 | Dashboard layout finished

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -1,53 +1,171 @@
 <script>
   import { Svg } from '$lib';
+  import { fade, fly } from 'svelte/transition';
+  import { page } from '$app/stores';
+  import { navigating } from '$app/stores';
 
-	let { children } = $props();
+  let isNavOpen = $state(false);
+  let transitioning = $state(false);
+  let { data, children } = $props();
+
+  const toggleNav = (e) => {
+    if (window.innerWidth < 1024) {
+      isNavOpen = !isNavOpen;
+    }
+  }
+
+  const active = (path) => {
+    return $page.url.pathname === path;
+  }
 </script>
 
-<main>
+<main class:open={isNavOpen}>
   <nav>
+    <button onclick={toggleNav} aria-label="Toggle Navigation">
+      <span></span>
+      <span></span>
+      <span></span>
+    </button>
     <a href="/"><img src="/images/Logo-lauralisa.nl-wit.png" alt="LAURALISA logo" /></a>
-    <ul>
-      <li><a href="/dashboard">
-        
-        <span class="icon">
-          <Svg name="dashboard" size="18" color="currentColor"/>
-        </span>
-        Dashboard</a></li>
-      <li><a href="/dashboard/pages">Paginas</a></li>
-      <li><a href="/dashboard/menus">Menus</a></li>
-      <li><a href="/dashboard/media">Media</a></li>
-      <li><a href="/dashboard/settings">Instellingen</a></li>
-    </ul>
 
+    <ul>
+      <li><a href="/dashboard" onclick={toggleNav} class:active={active('/dashboard')}>
+        <span class="icon"><Svg name="dashboard" size="18" color="currentColor"/></span>
+        Dashboard
+      </a></li>
+
+      <li><a href="/dashboard/pages" onclick={toggleNav} class:active={active('/dashboard/pages')}>
+        <span class="icon"><Svg name="pages" scale=".9" color="currentColor"/></span>
+        Paginas
+      </a></li>
+
+      <li><a href="/dashboard/menus" onclick={toggleNav} class:active={active('/dashboard/menus')}>
+        <span class="icon"><Svg name="menus" size="18" color="currentColor"/></span>
+        Menus
+      </a></li>
+
+      <li><a href="/dashboard/media" onclick={toggleNav} class:active={active('/dashboard/media')}>
+        <span class="icon"><Svg name="media" size="18" color="currentColor"/></span>
+        Media
+      </a></li>
+
+      <li><a href="/dashboard/settings" onclick={toggleNav} class:active={active('/dashboard/settings')}>
+        <span class="icon"><Svg name="settings" scale=".8" color="currentColor"/></span>
+        Instellingen
+      </a></li>
+    </ul>
   </nav>
 
-  <section>
-    {@render children()}
-  </section>
+  <article>
+    {#key data.url}
+      <section
+        class:transitioning={transitioning}
+        in:fade={{ duration: 300, delay: 400 }}
+        out:fade={{ duration: 400 }}
+        onintrostart={() => transitioning = true}
+        onoutroend={() => transitioning = false}
+      >
+        {@render children()}
+      </section>
+    {/key}
+  </article>
 </main>
 
 <style>
+
   main {
+    --nav-width: clamp(280px, 30vw + 5rem, 400px);
     height: 100%;
     width: 100%;
     display: flex;
     gap: 1rem;
     padding: 1rem;
+    transition: transform .5s ease-in-out;
+    &.open {
+      transform: translateX(var(--nav-width));
+    }
   }
 
   main nav {
-    padding: .25rem 0 .25rem 1.25rem;
+    position: absolute;
+    background-color: var(--dusty-rose);
+    transform: translateX(-100%);
+    width: var(--nav-width);
+    z-index: 1;
+
+    @media (min-width: 1024px) {
+      background-color: transparent;
+      position: relative;
+      transform: none;
+      padding: .25rem 0 .25rem 1.25rem;
+      display: flex;
+      flex-direction: column;
+      flex: 1 0 20%;
+    }
+  }
+
+  main nav > button {
+    --radius: 1rem;
+    position: absolute;
+    top: -1px;
+    right: 1px;
+    transform: translateX(100%);
     display: flex;
     flex-direction: column;
-    flex: 1 0 20%
+    justify-content: space-between;
+    background: var(--dusty-rose);
+    border: none;
+    border-radius: 0 0 var(--radius) 0;
+    cursor: pointer;
+    height: 3rem;
+    width: 3.5rem;
+    padding: .9rem;
+    z-index: 2;
+
+    span {
+      width: 100%;
+      height: 3px;
+      background: var(--pure-white);
+      border-radius: 1rem;
+    }
+
+    &::before,
+    &::after {
+      content: '';
+      position: absolute;
+      width: var(--radius);
+      aspect-ratio: 1;
+      background-image: 
+        radial-gradient(
+          circle at 100% 100%,
+          transparent var(--radius), 
+          var(--dusty-rose, white)
+          var(--radius));
+    }
+
+    &::before {
+      top: 0;
+      right: 0;
+      transform: translateX(100%);
+    }
+
+    &::after {
+      bottom: -1rem;
+      left: -1rem;
+      transform: translateX(100%);
+    }
+
+    @media (min-width: 1024px) {
+      display: none;
+    }
   }
 
   main nav > a {
-    width: 60%;
+    width: 100%;
+    display: block;
 
     img {
-      width: 100%;
+      width: 80%;
     }
   }
 
@@ -74,9 +192,10 @@
         transition: all 0.3s ease-in-out;
       }
 
-      &:hover {
+      &:hover,
+      &.active {
         color: var(--blush-veil);
-        margin-left: .5rem;
+        margin-left: 1rem;
 
         span {
           color: var(--blush-veil);
@@ -85,10 +204,31 @@
     }
   }
 
-  main section {
+  main article {
+    position: relative;
     background-color: var(--pure-white);
-    padding: 1.25rem;
-    flex: 0 1 80%;
     border-radius: 1.25rem;
+    flex: 0 1 100%;
+
+    @media (min-width: 1024px) {
+      flex: 0 1 80%;
+    }
+  }
+
+  main article section {
+    position: absolute;
+    inset: 0;
+    padding: 4rem 1.5rem;
+    width: 100%;
+    height: 100%;
+    overflow: auto;
+
+    @media (min-width: 1024px) {
+      padding: 1.25rem;
+    }
+  }
+
+  main section.transitioning {
+    overflow: hidden;
   }
 </style>
--- src/routes/dashboard/account/+page.svelte
+++ src/routes/dashboard/account/+page.svelte
@@ -1,1 +1,15 @@
-Account page!
+<main>
+  <h1>Account page!</h1>
+</main>
+
+<style>
+  main {
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    gap: 1rem;
+    color: var(--deep-charcoal);
+  }
+</style>
