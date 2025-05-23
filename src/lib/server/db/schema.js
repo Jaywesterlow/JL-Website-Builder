8a8217af2be49c84722f7547fd39bbcd6cdd4069
Jaymar Westerlow <jaywesterlow71@gmail.com>
2025-05-23 14:39:06 +0800

    v5.2 (Media table) | Reset DB, added media table

--- src/lib/server/db/schema.js
+++ src/lib/server/db/schema.js
@@ -1,10 +1,17 @@
 import { mysqlTable, varchar, text, int, timestamp } from "drizzle-orm/mysql-core";
 
-// 1️⃣ USERS TABLE (Store user accounts)
+//* USERS TABLE
 export const users = mysqlTable("users", {
   id: int("id").autoincrement().primaryKey(),
   name: varchar("name", { length: 255 }).notNull(),
   email: varchar("email", { length: 255 }).notNull().unique(),
   password_hash: text("password_hash").notNull(),
   created_at: timestamp("created_at").defaultNow(),
+});
+
+//* MEDIA TABLE
+export const media = mysqlTable("media", {
+  id: int("id").autoincrement().primaryKey(),
+  userId: int("user_id").notNull(),
+  media: varchar("media", { length: 255 }).notNull(),
 });
