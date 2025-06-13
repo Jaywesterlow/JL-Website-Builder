a965d21aa77ffc4306396ff2572e0bec651c971e
Jaymar Westerlow <jaywesterlow71@gmail.com>
2025-06-13 13:43:06 +0800

    v7.0 (fix) | Playwright update

--- playwright.config.js
+++ playwright.config.js
@@ -13,7 +13,7 @@ export default defineConfig({
   reporter: 'html',
   use: {
     trace: 'on-first-retry',
-    baseURL: 'http://localhost:5173'
+    baseURL: process.env.BASE_URL || 'http://localhost:3000'
   },
 
   projects: [
